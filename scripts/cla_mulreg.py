Refactor cla_mulreg.py to remove regularization of bias term and update models_comparison.py to fix variable naming in error calculation

scripts/cla_mulreg.py
@@ -15,7 +15,7 @@ def estimate_weights(X_train, y_train, opt_lambda, M):
 
     # Estimate weights for the optimal value of lambda, on entire training set
     lambdaI = opt_lambda * np.eye(M)
-    lambdaI[0, 0] = 0  # Do no regularize the bias term
+    lambdaI[0, 0] = 0
     return np.linalg.solve(XtX + lambdaI, Xty).squeeze()
 
 def fit(X_train, y_train, max_iter=1000, regularization=1):

scripts/models_comparison.py
@@ -134,9 +134,9 @@ for i, (outer_train_index, outer_test_index) in enumerate(outer_cv.split(X, y)):
             # Determine errors
             e = np.zeros_like(y_test)
             # Determine errors
-            for i in range(len(y_test_est)):
-                if y_test_est[i] != y_test[i]:
-                    e[i] = 1
+            for j in range(len(y_test_est)):
+                if y_test_est[j] != y_test[j]:
+                    e[j] = 1
 
             # The error rate for the ANN model is stored.
             inner_ann_error_rate.append(np.mean(e).round(2))
@@ -229,9 +229,9 @@ for i, (outer_train_index, outer_test_index) in enumerate(outer_cv.split(X, y)):
 
     # Determine errors
     e = np.zeros_like(outer_y_test)
-    for i in range(len(yhat_ann)):
-        if yhat_ann[i] != outer_y_test[i]:
-            e[i] = 1
+    for j in range(len(yhat_ann)):
+        if yhat_ann[j] != outer_y_test[j]:
+            e[j] = 1
     ann_error_rate = np.mean(e).round(2)
     ann_error_rates.append(ann_error_rate)
     n_hidden_units_best.append(n_hidden_unit_best)