c20f5c0abaf888671abbe3064de29939217428b5
soerengraae <soerengraae@live.dk>
2024-04-10 19:07:45 +0200

    Update cla_mulreg.py to use 'liblinear' solver in LogisticRegression

--- scripts/cla_mulreg.py
+++ scripts/cla_mulreg.py
@@ -5,7 +5,7 @@ A regularization parameter is used to avoid overfitting.
 from sklearn.linear_model import LogisticRegression
 
 def fit(X_train, y_train, max_iter=1000, regularization=1):
-    model = LogisticRegression(multi_class='multinomial', random_state=0, max_iter=max_iter, C=regularization)
+    model = LogisticRegression(multi_class='multinomial', solver='liblinear', random_state=0, max_iter=max_iter, C=regularization)
 
     # Train the multimonial regression model
     model.fit(X_train, y_train)
